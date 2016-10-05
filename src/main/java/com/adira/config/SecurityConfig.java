e11d9d8d536e1089de8e3751159b8546b0246523
Didi Yudha Perwira <didi@realtimejakarta.com>
2016-10-05 20:10:04 +0700

    temporary

---
+++ src/main/java/com/adira/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.adira.config;
+
+/**
+ * Created by didi-realtime on 05/10/16.
+ */
+public class SecurityConfig {
+}
---
+++ src/main/java/com/adira/entity/User.java
@@ -0,0 +1,7 @@
+package com.adira.entity;
+
+/**
+ * Created by didi-realtime on 05/10/16.
+ */
+public class User {
+}
