fb5ed26bdd9573dae7671444566dea117a2cf4d5
Didi Yudha Perwira <didi@realtimejakarta.com>
2016-11-10 02:48:02 +0700

    adding styling in table list of audits

--- src/main/java/com/adira/controller/AuditController.java
+++ src/main/java/com/adira/controller/AuditController.java
@@ -9,10 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.text.SimpleDateFormat;
@@ -52,7 +49,8 @@ public class AuditController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public void findAll(Model model) {
+    public void findAll(Model model, @RequestParam(value = "date", required = false) Date date) {
+        Date d = date;
         List<Audit> audits = auditService.findAll();
         model.addAttribute("audits", audits);
     }
